45c353f23d326da79b694fc2bd7938a199ee1807
Riya Agrahari <500052961@stu.upes.ac.in>
2019-03-06 17:00:52 +0530

    Update mapColouring.cpp

--- mapColouring.cpp
+++ mapColouring.cpp
@@ -1,3 +1,4 @@
+
 //Open GL Libraries
 #include<iostream>
 #include<GL/glut.h>
@@ -7,13 +8,13 @@
 #define RED 1
 #define GREEN 2
 #define BLUE 3
-#define ORANGE 4
 
 using namespace std;
 
+
 int rightClickx, rightClicky;
-int R = 0, G = 0, B = 0, cityCount = 0, lineCount = 0, first = 0,  x1 = 0,x2 = 0, y1 = 0, y2 = 0;
-int cityInfo[10][5];
+int R = 0, G = 0, B = 0, cityCount = 0, lineCount = 0, first = 0,  x1 = 0,x2 = 0, y1 = 0, y2 = 0, id = 0;
+int cityInfo[10][9];
 int connection[10][10] = {
 	{0,1,0,0,0,0,0,0,0,0},
 	{1,0,1,0,0,0,1,0,0,0},
@@ -30,7 +31,9 @@ int connection[10][10] = {
 void floodFill(int, int);
 void makeCity(int,int);
 void line(int, int,int,int);
+int checkId(int , int);
 int createGLUTMenus();
+void checkAdjacent(int);
 
 void Init(){
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -42,12 +45,16 @@ void Init(){
 void mouse(int button, int press, int mx, int my){
 	if (button == GLUT_LEFT_BUTTON && press == GLUT_DOWN){
 		if (cityCount < 10 && lineCount == 0){
-			makeCity(mx, 650-my );
+			makeCity(mx, 650-my);
 			cityInfo[cityCount][0] = cityCount;
 			cityInfo[cityCount][1] = mx - 60;
 			cityInfo[cityCount][2] = 650-my - 30;
 			cityInfo[cityCount][3] = mx + 60;
 			cityInfo[cityCount][4] = 650-my + 30;
+			cityInfo[cityCount][5] = 0;
+			cityInfo[cityCount][6] = 1;
+			cityInfo[cityCount][7] = 1;
+			cityInfo[cityCount][8] = 1;
 			cityCount++;
 			cout<<"No of city: "<<cityCount<<endl;
 		}
@@ -75,7 +82,7 @@ void mouse(int button, int press, int mx, int my){
 		}
 		else{
 			for (int i = 0; i < 10; i++){
-				for (int j = 0; j < 5; j++){
+				for (int j = 0; j < 9; j++){
 					cout<< cityInfo[i][j]<<" ";
 				}
 				cout<<endl;
@@ -86,7 +93,9 @@ void mouse(int button, int press, int mx, int my){
 	else if (button == GLUT_RIGHT_BUTTON && press == GLUT_DOWN){
 			rightClickx = mx ;
 			rightClicky = 650-my;
+			id = checkId(rightClickx, rightClicky);			
 			createGLUTMenus();
+			
 	}
 }
 
@@ -117,6 +126,22 @@ void line(int a, int b,int c,int d)
  glFlush();
 }
 
+int checkId(int x, int y)
+{
+	for(int i=0;i<10;i++)
+	{
+		if (x >= cityInfo[i][1] && y >= cityInfo[i][2] && x <= cityInfo[i][3] && y <= cityInfo[i][4])
+			{
+				cout<<"City Id = "<<i<<endl;
+				cout<<"City color"<<cityInfo[id][5]<<endl;
+				cout<<"City color R"<<cityInfo[id][6]<<endl;	
+				cout<<"City color G"<<cityInfo[id][7]<<endl;	
+				cout<<"City color B"<<cityInfo[id][8]<<endl;					
+				return i;
+			}	
+	
+	}
+}
 void processMenuEvents(int option) {
 
 float red, green, blue;
@@ -126,26 +151,56 @@ float red, green, blue;
 			cout<<"Red is selected"<<endl;
 			R = 0, G = 0, B = 0;
 			glColor3f(1,0,0);
+			cityInfo[id][5] = 1;
+			//cityInfo[id][6] = 1;			
 			floodFill(rightClickx, rightClicky);
-			
+			checkAdjacent(id);
 			break;
 		case GREEN :
 			cout<<"Green is selected"<<endl;
 			R = 0, G = 0, B = 0;
 			glColor3f(0,1,0);
+			cityInfo[id][5] = 2;
+			//cityInfo[id][7] = 1;
 			floodFill(rightClickx, rightClicky);
+			checkAdjacent(id);
 			//glutDetachMenu(GLUT_RIGHT_BUTTON);
 			break;
 		case BLUE :
 			cout<<"Blue is selected"<<endl;
 			R = 0, G = 0, B = 0;
 			glColor3f(0,0,1);
+			cityInfo[id][5] = 3;
+			//cityInfo[id][8] = 1;
 			floodFill(rightClickx, rightClicky);
+			checkAdjacent(id);
 			//glutDetachMenu(GLUT_RIGHT_BUTTON);
 			break;
 	}
 }
+void checkAdjacent(int city_id)
+{
+	for(int i =0;i<10;i++)
+	{
+		int color = cityInfo[city_id][5];
 
+		if(connection[city_id][i] == 1)
+		{
+                  if (color == 1)
+			{
+				cityInfo[i][6]=-1;
+			}
+		  if (color == 2)
+			{
+				cityInfo[i][7]=-1;
+			}
+		  if (color == 3)
+			{
+				cityInfo[i][8]=-1;
+			}
+		}
+	}
+}
 int createGLUTMenus() {
 
 	int menu;
@@ -154,11 +209,23 @@ int createGLUTMenus() {
 	// tell glut that "processMenuEvents" will
 	// handle the events
 	menu = glutCreateMenu(processMenuEvents);
-	
+                  if (cityInfo[id][6] != -1)
+			{
+				glutAddMenuEntry("RED",RED);
+			}
+		  if (cityInfo[id][7] != -1)
+			{
+				glutAddMenuEntry("GREEN",GREEN);
+			}
+		  if (cityInfo[id][8] != -1)
+			{
+				
+				glutAddMenuEntry("BLUE",BLUE);
+			}
 	//add entries to our menu
-	glutAddMenuEntry("Red",RED);
-	glutAddMenuEntry("Green",GREEN);
-	glutAddMenuEntry("Blue",BLUE);
+	
+	
+	
 
 	// attach the menu to the right button
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
@@ -210,7 +277,7 @@ int main(int argc, char **argv){
 	glutInit(&argc, argv);//Initialize window
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB); //Single Window
 	glutInitWindowSize(1050, 650); //Initialize the size of Window
-	glutInitWindowPosition(100, 0); //Initial position of the window
+	glutInitWindowPosition(0, 0); //Initial position of the window
 	glutCreateWindow("Map Colouring"); //Name of the Window
 	Init();
 	glutMouseFunc(mouse);
@@ -219,3 +286,5 @@ int main(int argc, char **argv){
 	glutMainLoop();
 	return 0;
 }
+
+    
